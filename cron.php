e01ae27a64bc833b5afbc0bdf5d65634334d0902
Harley Bussell <harley_bussell@flightcentre.com>
2014-05-05 11:24:14 +1000

    use config.php in cron

--- cron.php
+++ cron.php
@@ -4,17 +4,7 @@ require_once __DIR__.'/vendor/autoload.php';
 
 $app = new Silex\Application();
 
-
-$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
-    'db.options' => array(
-        'driver'   => 'pdo_mysql',
-        'host'     => 'localhost',
-        'dbname'     => 'relhub_jenkins',
-        'user'     => 'root',
-        'password'     => 'WebSols',
-    ),
-));
-
+require_once __DIR__.'/config.php';
 
 $sql = "SELECT * FROM build ";
 $builds = $app['db']->fetchAssoc($sql);
